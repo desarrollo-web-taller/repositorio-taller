75a3295104d2ef2f2b8c9182351ae0bfa49d75e2
Gabriel Menacho <gabo.menacho@gmail.com>
2021-04-24 12:43:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import App2 from './App2';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <App2></App2>
       <header className="App-header">
+        <h2>Mi nueva pagina</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,4 +25,3 @@ function App() {
   );
 }
 
-export default App;
---
+++ src/App2.js
@@ -0,0 +1,6 @@
+export default function App2() {
+  return (
+		<div>Mi nuevo componente!</div>
+	);
+}
+
